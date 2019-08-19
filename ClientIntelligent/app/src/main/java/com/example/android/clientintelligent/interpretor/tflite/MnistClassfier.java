704ed45ea6618f2e9df9713aa5213c59b4a7dd98
David Xiang <xiangdongwei@pku.edu.cn>
2019-08-19 22:04:31 +0800

    add composite application

---
+++ ClientIntelligent/app/src/main/java/com/example/android/clientintelligent/interpretor/tflite/MnistClassfier.java
@@ -0,0 +1,82 @@
+package com.example.android.clientintelligent.interpretor.tflite;
+
+import android.app.Activity;
+
+import com.example.android.clientintelligent.interpretor.tflite.Classifier;
+
+import java.io.IOException;
+
+/** This TensorFlowLite classifier works with the float Mnist model. */
+public class MnistClassfier extends Classifier {
+    private String path;
+
+    /**
+     * An array to hold inference results, to be feed into Tensorflow Lite as outputs. This isn't part
+     * of the super class, because we need a primitive array here.
+     */
+    private float[][] labelProbArray = null;
+
+    /**
+     * Initializes a {@code ClassifierFloatMobileNet}.
+     *
+     * @param activity
+     */
+    public MnistClassfier(Activity activity, String path) throws IOException {
+        super(activity, path);
+        this.path = path;
+        labelProbArray = new float[1][getNumLabels()];
+    }
+
+    @Override
+    public int getImageSizeX() {
+        return 28;
+    }
+
+    @Override
+    public int getImageSizeY() {
+        return 28;
+    }
+
+    @Override
+    protected String getModelPath() {
+        // you can download this file from
+        // see build.gradle for where to obtain this file. It should be auto
+        // downloaded into assets.
+        return path;
+    }
+
+    @Override
+    protected String getLabelPath() {
+        return "labels/mnist.txt";
+    }
+
+    @Override
+    protected int getNumBytesPerChannel() {
+        return 4; // Float.SIZE / Byte.SIZE;
+    }
+
+    @Override
+    protected void addPixelValue(int pixelValue) {
+        imgData.putFloat((pixelValue & 0xFF) / 255.f);
+    }
+
+    @Override
+    protected float getProbability(int labelIndex) {
+        return labelProbArray[0][labelIndex];
+    }
+
+    @Override
+    protected void setProbability(int labelIndex, Number value) {
+        labelProbArray[0][labelIndex] = value.floatValue();
+    }
+
+    @Override
+    protected float getNormalizedProbability(int labelIndex) {
+        return labelProbArray[0][labelIndex];
+    }
+
+    @Override
+    protected void runInference() {
+        tflite.run(imgData, labelProbArray);
+    }
+}
